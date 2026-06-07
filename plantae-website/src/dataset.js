823d64cfe78a69467cca53f3f4373c48bff52af6
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2026-06-07 17:36:31 +0400

    updated

---
+++ plantae-website/src/dataset.js
@@ -0,0 +1,174 @@
+const plantsdata = {
+     /*------------------------------------ Spring--------------------------*/
+    "Spring":[
+        {
+            "id":"Lettuce",
+            "name":"Lettuce",
+            "image":"plantae-website/public/assets/Spring/lettuce.jpg",
+            "Sunlight_Hours":"6",
+            "Water_Requirement":"Medium",
+            "Soil_Type":"Well-drained soil",
+            "Planting_Season":"Spring",
+            "Harvest_Time":"30-60 days"
+            
+        },
+        {
+            "id":"Spinach",
+            "name":"Spinach",
+            "image":"plantae-website/public/assets/Spring/spinach.jpg",
+            "Sunlight_Hours":"4-6",
+            "Water_Requirement":"Medium",
+            "Soil_Type":"Rich, well-drained soil",
+            "Planting_Season":"Spring",
+            "Harvest_Time":"25-45 days"
+        },
+        {
+            "id":"Peas",
+            "name":"Peas",
+            "image":"plantae-website/public/assets/Spring/peas.jpg",
+            "Sunlight_Hours":"6-8",
+            "Water_Requirement":"Medium",
+            "Soil_Type":"Well-drained soil",
+            "Planting_Season":"Spring",
+            "Harvest_Time":"60-70 days"
+        },
+        {
+            "id":"Carrots",
+            "name":"Carrots",
+            "image":"plantae-website/public/assets/Spring/carrot.png", 
+            "Sunlight_Hours":"6-8",
+            "Water_Requirement":"Medium",
+            "Soil_Type":"Loose, well-drained soil",
+            "Planting_Season":"Spring",
+            "Harvest_Time":"70-80 days"
+        },
+        {
+            "id":"Radishes",
+            "name":"Radishes",
+            "image":"plantae-website/public/assets/Spring/radish.jpeg",
+            "Sunlight_Hours":"6-8",
+            "Water_Requirement":"Medium",   
+            "Soil_Type":"Well-drained soil",
+            "Planting_Season":"Spring",
+            "Harvest_Time":"20-30 days"
+        },
+        {
+        "id":"potatoes",
+        "name":"Potatoes",
+        "image":"plantae-website/public/assets/Spring/potatoes.jpg",
+        "Sunlight_Hours":"6-8",
+        "Water_Requirement":"Medium",
+        "Soil_Type":"Well-drained soil",
+        "Planting_Season":"Spring",
+        "Harvest_Time":"70-120 days"
+        },
+        {
+            "id":"daffodils",
+            "name":"Daffodils",
+            "image":"plantae-website/public/assets/Spring/daffodils.jpeg",
+            "Sunlight_Hours":"6-8",
+            "Water_Requirement":"Medium",
+            "Soil_Type":"Well-drained soil",
+            "Planting_Season":"Spring",
+            "Harvest_Time":"90-120 days"
+        },
+        {
+            "id":"tulips",
+            "name":"Tulips",
+            "image":"plantae-website/public/assets/Spring/tulips.jpg",
+            "Sunlight_Hours":"6-8",
+            "Water_Requirement":"Medium",
+            "Soil_Type":"Well-drained soil",    
+            "Planting_Season":"Spring",
+            "Harvest_Time":"90-120 days"    
+        }
+
+
+    ],
+     "Summer":[
+        {
+          "id":"Tomatoes",
+          "name":"Tomatoes",
+          "image":"plantae-website/public/assets/Summer/tomatoes.jpg",
+            "Sunlight_Hours":"6-8",
+            "Water_Requirement":"Medium",
+            "Soil_Type":"Well-drained soil",
+            "Planting_Season":"Summer",
+            "Harvest_Time":"60-80 days"
+            
+        },
+        {
+            "id":"Spinach",
+            "name":"Spinach",
+            "image":"plantae-website/public/assets/Spring/spinach.jpg",
+            "Sunlight_Hours":"4-6",
+            "Water_Requirement":"Medium",
+            "Soil_Type":"Rich, well-drained soil",
+            "Planting_Season":"Spring",
+            "Harvest_Time":"25-45 days"
+        },
+        {
+            "id":"Peas",
+            "name":"Peas",
+            "image":"plantae-website/public/assets/Spring/peas.jpg",
+            "Sunlight_Hours":"6-8",
+            "Water_Requirement":"Medium",
+            "Soil_Type":"Well-drained soil",
+            "Planting_Season":"Spring",
+            "Harvest_Time":"60-70 days"
+        },
+        {
+            "id":"Carrots",
+            "name":"Carrots",
+            "image":"plantae-website/public/assets/Spring/carrot.png", 
+            "Sunlight_Hours":"6-8",
+            "Water_Requirement":"Medium",
+            "Soil_Type":"Loose, well-drained soil",
+            "Planting_Season":"Spring",
+            "Harvest_Time":"70-80 days"
+        },
+        {
+            "id":"Radishes",
+            "name":"Radishes",
+            "image":"plantae-website/public/assets/Spring/radish.jpeg",
+            "Sunlight_Hours":"6-8",
+            "Water_Requirement":"Medium",   
+            "Soil_Type":"Well-drained soil",
+            "Planting_Season":"Spring",
+            "Harvest_Time":"20-30 days"
+        },
+        {
+        "id":"potatoes",
+        "name":"Potatoes",
+        "image":"plantae-website/public/assets/Spring/potatoes.jpg",
+        "Sunlight_Hours":"6-8",
+        "Water_Requirement":"Medium",
+        "Soil_Type":"Well-drained soil",
+        "Planting_Season":"Spring",
+        "Harvest_Time":"70-120 days"
+        },
+        {
+            "id":"daffodils",
+            "name":"Daffodils",
+            "image":"plantae-website/public/assets/Spring/daffodils.jpeg",
+            "Sunlight_Hours":"6-8",
+            "Water_Requirement":"Medium",
+            "Soil_Type":"Well-drained soil",
+            "Planting_Season":"Spring",
+            "Harvest_Time":"90-120 days"
+        },
+        {
+            "id":"tulips",
+            "name":"Tulips",
+            "image":"plantae-website/public/assets/Spring/tulips.jpg",
+            "Sunlight_Hours":"6-8",
+            "Water_Requirement":"Medium",
+            "Soil_Type":"Well-drained soil",    
+            "Planting_Season":"Spring",
+            "Harvest_Time":"90-120 days"    
+        }
+
+
+    ],
+
+}
